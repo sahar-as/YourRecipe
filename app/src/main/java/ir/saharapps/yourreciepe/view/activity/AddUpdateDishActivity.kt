df6feac1786e36cd38fbc60856ae60da667458a0
sahar-as <sahar.asadian90@gmail.com>
2022-05-21 12:44:01 +0430

    implementing add updateDish activity and its xml

--- app/src/main/java/ir/saharapps/yourreciepe/view/activity/AddUpdateDishActivity.kt
+++ app/src/main/java/ir/saharapps/yourreciepe/view/activity/AddUpdateDishActivity.kt
@@ -2,11 +2,39 @@ package ir.saharapps.yourreciepe.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import ir.saharapps.yourreciepe.R
+import ir.saharapps.yourreciepe.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var binding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpActionBar()
+        binding.imgAddUpdateDishAddImageButton.setOnClickListener(this)
+    }
+
+    private fun setUpActionBar(){
+        setSupportActionBar(binding.toolbarAddUpdateDish)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarAddUpdateDish.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.img_addUpdateDish_addImageButton ->{
+                    Toast.makeText(this, "you pressed it", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
     }
 }
