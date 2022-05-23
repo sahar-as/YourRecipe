80a2ee6c653bc8f7f881a871ec524ec9e4775e55
sahar-as <sahar.asadian90@gmail.com>
2022-05-23 11:20:12 +0430

    addUpdateDishActivity selecting image from Gallery and camera

--- app/src/main/java/ir/saharapps/yourreciepe/view/activity/AddUpdateDishActivity.kt
+++ app/src/main/java/ir/saharapps/yourreciepe/view/activity/AddUpdateDishActivity.kt
@@ -1,14 +1,54 @@
 package ir.saharapps.yourreciepe.view.activity
 
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.karumi.dexter.Dexter
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import ir.saharapps.yourreciepe.R
 import ir.saharapps.yourreciepe.databinding.ActivityAddUpdateDishBinding
+import ir.saharapps.yourreciepe.databinding.DialogChoosingImageBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener{
+    private val TAG = "AddUpdateDishActivity"
     private lateinit var binding: ActivityAddUpdateDishBinding
+
+    private var mImagePath: String =""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +71,169 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener{
         if(v != null){
             when(v.id){
                 R.id.img_addUpdateDish_addImageButton ->{
-                    Toast.makeText(this, "you pressed it", Toast.LENGTH_SHORT).show()
+                    selectImageOfDish()
                     return
                 }
             }
         }
     }
+
+    private fun selectImageOfDish(){
+        val dialog = Dialog(this)
+        val dialogBinding: DialogChoosingImageBinding = DialogChoosingImageBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialogBinding.txtDialogChooseImageGallery.setOnClickListener {
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .withListener(object :PermissionListener {
+                    override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        resultLauncherGallery.launch(intent)
+                    }
+
+                    override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                        Toast.makeText(
+                            this@AddUpdateDishActivity,
+                            "onPermissionDenied from Gallery",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: PermissionRequest?,
+                        p1: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+            dialog.dismiss()
+        }
+        dialogBinding.txtDialogChooseImageCamera.setOnClickListener {
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+                                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                                resultLauncherCamera.launch(intent)
+                            }
+                        }
+
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    var resultLauncherCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.extras?.let {
+                val data: Bitmap = result.data!!.extras!!.get("data") as Bitmap
+//                binding.imgAddUpdateDishFoodImage.setImageBitmap(data)
+                Glide.with(this)
+                    .load(data)
+                    .centerCrop()
+                    .into(binding.imgAddUpdateDishFoodImage)
+
+                mImagePath = saveImageToInternalStorage(data)
+                Log.d(TAG, "hhhhhhhhhhhhhhh path $mImagePath")
+
+                binding.imgAddUpdateDishAddImageButton.setImageDrawable(
+                    ContextCompat.getDrawable(this,R.drawable.ic_edit))
+            }
+        }
+    }
+
+    var resultLauncherGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.let {
+                val data = result.data!!.data
+//                binding.imgAddUpdateDishFoodImage.setImageURI(data)
+                Glide.with(this)
+                    .load(data)
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .addListener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e(TAG, "onLoadFailed: Errroooorr occurred", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean
+                        ): Boolean {
+                            resource?.let {
+                                val bitmap: Bitmap = resource.toBitmap()
+                                mImagePath = saveImageToInternalStorage(bitmap)
+                                Log.d(TAG, "onResourceReady: hhhhhhhhhhhhhhh path $mImagePath")
+                            }
+                            return false
+                        }
+                    })
+                    .into(binding.imgAddUpdateDishFoodImage)
+                binding.imgAddUpdateDishAddImageButton.setImageDrawable(
+                    ContextCompat.getDrawable(this,R.drawable.ic_edit))
+            }
+        }
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap): String {
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(Companion.IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return file.absolutePath
+    }
+
+    companion object {
+        const val IMAGE_DIRECTORY = "FavDishImages"
+    }
 }
+
+
+//icon colors #306880
