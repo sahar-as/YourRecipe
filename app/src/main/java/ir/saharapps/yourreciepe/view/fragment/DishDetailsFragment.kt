09603e5cfa8b7f3ecfdf09c76dd5a0dc4dd90b6f
sahar-as <sahar.asadian90@gmail.com>
2022-05-26 10:03:56 +0430

    implement palette for dish detail page

--- app/src/main/java/ir/saharapps/yourreciepe/view/fragment/DishDetailsFragment.kt
+++ app/src/main/java/ir/saharapps/yourreciepe/view/fragment/DishDetailsFragment.kt
@@ -1,15 +1,28 @@
 package ir.saharapps.yourreciepe.view.fragment
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import ir.saharapps.yourreciepe.R
+import ir.saharapps.yourreciepe.databinding.FragmentDishDetailsBinding
+import java.io.IOException
+import java.util.*
 
 class DishDetailsFragment : Fragment() {
+
+    private var mBinding: FragmentDishDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +32,53 @@ class DishDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        mBinding = FragmentDishDetailsBinding.inflate(inflater,container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        Log.d("TAG", "onViewCreated: 111111111111 " + args.dishDetails.title)
+
+        args.let {
+            try {
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .listener(object: RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?, model: Any?, target: Target<Drawable>?,
+                            isFirstResource: Boolean): Boolean {
+                            Log.e("TAG", "onLoadFailed: Error for Loading Image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?, model: Any?, target: Target<Drawable>?,
+                            dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate(){
+                                    Palette ->
+                                    val paletteColor = Palette?.lightMutedSwatch?.rgb ?:0
+                                    mBinding!!.relativeLayoutDishDetail.setBackgroundColor(paletteColor)
+                                }
+                            }
+                            return false
+                        }
+                    } )
+                    .into(mBinding!!.imgDishImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            mBinding!!.txtFavDetailsTitle.text = it.dishDetails.title
+            mBinding!!.txtFavDetailsType.text =
+                it.dishDetails.type.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
+            mBinding!!.txtFavDetailsCategory.text = it.dishDetails.Category
+            mBinding!!.txtFavDetailsIngredients.text = it.dishDetails.ingredients
+            mBinding!!.txtFavDetailsCookingDirection.text = it.dishDetails.directionToCook
+            mBinding!!.txtFavDetailsCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+        }
     }
 }
