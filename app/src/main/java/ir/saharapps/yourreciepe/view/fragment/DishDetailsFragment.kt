79e92a735c058d64139cf14e437a7fe8bb845050
sahar-as <sahar.asadian90@gmail.com>
2022-05-30 13:07:33 +0430

    implement share icon and action for detail dish fragment

--- app/src/main/java/ir/saharapps/yourreciepe/view/fragment/DishDetailsFragment.kt
+++ app/src/main/java/ir/saharapps/yourreciepe/view/fragment/DishDetailsFragment.kt
@@ -1,12 +1,13 @@
 package ir.saharapps.yourreciepe.view.fragment
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.viewModels
@@ -20,6 +21,8 @@ import com.bumptech.glide.request.target.Target
 import ir.saharapps.yourreciepe.Application.FavDishApplication
 import ir.saharapps.yourreciepe.R
 import ir.saharapps.yourreciepe.databinding.FragmentDishDetailsBinding
+import ir.saharapps.yourreciepe.model.entities.FavDish
+import ir.saharapps.yourreciepe.utils.Constants
 import ir.saharapps.yourreciepe.viewmodel.FavDishViewModel
 import ir.saharapps.yourreciepe.viewmodel.FavDishViewModelFactory
 import java.io.IOException
@@ -28,6 +31,7 @@ import java.util.*
 class DishDetailsFragment : Fragment() {
 
     private var mBinding: FragmentDishDetailsBinding? = null
+    private var mFavDishDetails: FavDish? = null
 
     private val mFavDishViewModel: FavDishViewModel by viewModels {
         FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
@@ -35,6 +39,7 @@ class DishDetailsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -48,6 +53,7 @@ class DishDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
+        mFavDishDetails = args.dishDetails
 
         args.let {
             try {
@@ -110,5 +116,55 @@ class DishDetailsFragment : Fragment() {
                     requireActivity(),R.drawable.ic_favorite_unselected))
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_share, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_share_item ->{
+                val type = "text/plain"
+                val subject = "Checkout this dish recipe"
+                var extraText = ""
+                val shareWith = "Share with"
+
+                mFavDishDetails?.let {
+                    var image = ""
+                    if (it.imageSource == Constants.DISH_IMAGE_SOURCE_ONLINE) {
+                        image = it.image
+                    }
+
+                    var cookingInstructions = ""
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        cookingInstructions = Html.fromHtml(
+                            it.directionToCook,
+                            Html.FROM_HTML_MODE_COMPACT
+                        ).toString()
+                    } else {
+                        @Suppress("DEPRECATION")
+                        cookingInstructions = Html.fromHtml(it.directionToCook).toString()
+                    }
+
+                    extraText =
+                        "$image \n" +
+                                "\n Title:  ${it.title} \n\n Type: ${it.type} \n\n Category: ${it.Category}" +
+                                "\n\n Ingredients: \n ${it.ingredients} \n\n Instructions To Cook: \n $cookingInstructions" +
+                                "\n\n Time required to cook the dish approx ${it.cookingTime} minutes."
+                }
+
+
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = type
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                intent.putExtra(Intent.EXTRA_TEXT, extraText)
+                startActivity(Intent.createChooser(intent, shareWith))
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
